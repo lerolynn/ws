c534dc649aed9570d8ed2acba924c841ca8aaeee
lerolynn <lerolynn@gmail.com>
2022-08-14 01:24:38 +0800

    Fixed path for eval semseg

--- pseudo_mask_coco/step/eval_sem_seg.py
+++ pseudo_mask_coco/step/eval_sem_seg.py
@@ -9,14 +9,14 @@ from PIL import Image
 def run(args):
     # dataset = VOCSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.voc12_root)
     # labels = [dataset.get_example_by_keys(i, (1,))[0] for i in range(len(dataset))]
-    ids = open('coco14/train14.txt').readlines()
+    ids = open('coco14/train2014_semseg.txt').readlines()
     ids = [i.split('\n')[0] for i in ids]
     preds = []
     labels = []
     n_img = 0
 
     for i, id in enumerate(ids):
-        label = np.array(Image.open('Dataset/coco_2014/coco_seg_anno/%s.png' % id))
+        label = np.array(Image.open('../data/coco2014/mask/train2014/%s.png' % id))
         cls_labels = imageio.imread(os.path.join(args.sem_seg_out_dir, id + '.png')).astype(np.uint8)
         cls_labels[cls_labels == 255] = 0
         preds.append(cls_labels.copy())
