c525ae9ff5dc20ab61cce1c821df956c5f9c4047
lerolynn <lerolynn@gmail.com>
2022-08-14 22:55:37 +0800

    update seg diff

--- pseudo_mask_coco/coco14/misc/segmentation_diff.py
+++ pseudo_mask_coco/coco14/misc/segmentation_diff.py
@@ -7,6 +7,10 @@ with open('../train2014.txt', 'r') as f:
 #     shortlist = set([line.strip() for line in f])
 shortlist = [os.path.basename(x).split(".")[0] for x in glob.glob("../../result/sem_seg/*.png")]
 
+with open('../train2014_semseg.txt', 'w') as f:
+    for item in shortlist:
+        f.write("%s\n" % item)
+
 notdone = list(set(longlist - set(shortlist)))
 
 with open('../train2014_notdone.txt', 'w') as fp:
