093f3c1b5269983e24dbd0bf6fdb863a3d9b4080
lerolynn <lerolynn@gmail.com>
2022-08-14 13:20:07 +0800

    added diff

---
+++ pseudo_mask_coco/coco14/misc/segmentation_diff.py
@@ -0,0 +1,12 @@
+with open('../train2014_250.txt', 'r') as f:
+    longlist = set([line.strip() for line in f])
+
+with open('../train2014_semseg.txt', 'r') as f:
+    shortlist = set([line.strip() for line in f])
+
+notdone = list(set(longlist - set(shortlist)))
+
+with open('../train2014_notdone.txt', 'w') as fp:
+    for item in notdone:
+        # write each item on a new line
+        fp.write("%s\n" % item)
