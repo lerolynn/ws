bb543cfc36f01d676ad07ddb497998b8a7ec74bf
lerolynn <lerolynn@gmail.com>
2022-08-14 19:55:10 +0800

    read from directory instead of file

--- pseudo_mask_coco/coco14/misc/segmentation_diff.py
+++ pseudo_mask_coco/coco14/misc/segmentation_diff.py
@@ -1,8 +1,11 @@
+import glob
+import os
 with open('../train2014.txt', 'r') as f:
     longlist = set([line.strip() for line in f])
 
-with open('../train2014_semseg.txt', 'r') as f:
-    shortlist = set([line.strip() for line in f])
+# with open('../train2014_semseg.txt', 'r') as f:
+#     shortlist = set([line.strip() for line in f])
+shortlist = [os.path.basename(x).split(".")[0] for x in glob.glob("../../result/sem_seg/*.png")]
 
 notdone = list(set(longlist - set(shortlist)))
 
