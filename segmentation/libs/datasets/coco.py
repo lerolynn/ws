000f166626c5868af7e9f4a940bcc9a0024a39d5
lerolynn <lerolynn@gmail.com>
2022-08-19 03:56:04 +0800

    updated pseudomask dataloader for coco

--- segmentation/libs/datasets/coco.py
+++ segmentation/libs/datasets/coco.py
@@ -36,15 +36,14 @@ class COCO(_BaseDataset):
         image_id = self.files[index]
         image_path = osp.join(self.root, "JPEGImages", self.split, "COCO_train{}_".format(self.year) + image_id + ".jpg")
         label_path = osp.join(self.root, "mask", self.split, image_id + ".png")
-        print(image_path)
-        print(label_path)
+
         # Load an image and label
         image = cv2.imread(image_path, cv2.IMREAD_COLOR).astype(np.float32)
         label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
         return image_id, image, label
 
 class pseudoCOCO(_BaseDataset):
-    """COCO ground truth dataset"""
+    """COCO pseudo ground truth dataset"""
 
     def __init__(self, year=2014, **kwargs):
         self.year = year
@@ -54,11 +53,9 @@ class pseudoCOCO(_BaseDataset):
         self.root = osp.join(self.root, "coco{}".format(self.year))
         # Create data list by parsing the "images" folder
         if self.split in ["train2014", "val2014"]:
-            file_list = sorted(glob(osp.join(self.root, "JPEGImages", self.split, "*.jpg")))
-            assert len(file_list) > 0, "{} has no image".format(
-                osp.join(self.root, "JPEGImages", self.split)
-            )
-            file_list = [f.split("/")[-1].replace(".jpg", "") for f in file_list]
+            file_list = osp.join(self.root, "ImageSets", self.split + ".txt")
+            file_list = tuple(open(file_list, "r"))
+            file_list = [id_.strip() for id_ in file_list]
             self.files = file_list
         else:
             raise ValueError("Invalid split name: {}".format(self.split))
@@ -66,8 +63,9 @@ class pseudoCOCO(_BaseDataset):
     def _load_data(self, index):
         # Set paths
         image_id = self.files[index]
-        image_path = osp.join(self.root, "JPEGImages", self.split, image_id + ".jpg")
+        image_path = osp.join(self.root, "JPEGImages", self.split, "COCO_train{}_".format(self.year) + image_id + ".jpg")
         label_path = osp.join(self.root, "mask", self.split, image_id + ".png")
+
         # Load an image and label
         image = cv2.imread(image_path, cv2.IMREAD_COLOR).astype(np.float32)
         label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
