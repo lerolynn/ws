c05ab9aa152dd58720e12f37a9ca3acace0cf497
lerolynn <lerolynn@gmail.com>
2022-10-03 02:16:46 +0800

    run adv cam

--- pseudo_mask/run_sample.py
+++ pseudo_mask/run_sample.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     parser.add_argument("--log_name", default="log/voc12_sample_train_eval", type=str)
     parser.add_argument("--cam_weights_name", default="sess/voc12/res50_cam.pth", type=str)
     parser.add_argument("--irn_weights_name", default="sess/voc12/res50_irn.pth", type=str)
-    parser.add_argument("--cam_out_dir", default="result/voc12/cg_cam", type=str)
+    parser.add_argument("--cam_out_dir", default="result/voc12/cam", type=str)
     parser.add_argument("--prev_cam_out_dir", default="result/voc12/prev_cam", type=str)
     parser.add_argument("--ir_label_out_dir", default="result/voc12/ir_label", type=str)
     parser.add_argument("--sem_seg_out_dir", default="result/voc12/sem_seg", type=str)
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     # Step
     parser.add_argument("--train_cam_pass", default=True)
     # Train adversarial erasing step
-    parser.add_argument("--train_adv_pass", default=False)
+    parser.add_argument("--train_adv_pass", default=True)
 
     parser.add_argument("--make_cam_pass", default=True)
     parser.add_argument("--eval_cam_pass", default=True)
