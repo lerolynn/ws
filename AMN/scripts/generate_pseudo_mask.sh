fc61e9703c9a8eef9ef1ada2c78f5be3f6aa51ce
lerolynn <lerolynn@gmail.com>
2022-10-11 03:58:57 +0800

    Modified paths

--- AMN/scripts/generate_pseudo_mask.sh
+++ AMN/scripts/generate_pseudo_mask.sh
@@ -9,14 +9,17 @@ GPU=0,1,2,3
      --cam_eval_thres 0.15 \
      --eval_cam_pass True \
      --cam_to_ir_label_pass True \
-     --conf_fg_thres 0.30 \
+     --conf_fg_thres 0.40 \
      --conf_bg_thres 0.05 \
+     --cam_out_dir ../pseudo_mask/cgnet/output 
+
 
 # 2.1. train an attribute manipulation network
  CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
      --voc12_root ${DATASET_ROOT} \
      --num_workers 8 \
-     --train_amn_pass True
+     --train_amn_pass True \
+     --cam_out_dir ../pseudo_mask/cgnet/output 
 
 # 2.2. generate activation maps and refined seed for boundary refinement
 CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
@@ -26,17 +29,20 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
     --eval_amn_cam_pass True \
     --amn_cam_to_ir_label_pass True \
     --conf_fg_thres 0.45 \
-    --conf_bg_thres 0.15
+    --conf_bg_thres 0.15 \
+    --cam_out_dir ../pseudo_mask/cgnet/output 
 
 # 3.1. train a boundary refinement network (IRN)
 CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
     --voc12_root ${DATASET_ROOT} \
     --num_workers 8 \
-    --train_irn_pass True
+    --train_irn_pass True \
+    --cam_out_dir ../pseudo_mask/cgnet/output 
 
 # 3.2. generate the final pseudo-masks
 CUDA_VISIBLE_DEVICES=${GPU} python3 run_sample.py \
     --voc12_root ${DATASET_ROOT} \
     --num_workers 8 \
     --make_sem_seg_pass True \
-    --eval_sem_seg_pass True
+    --eval_sem_seg_pass True \
+    --cam_out_dir ../pseudo_mask/cgnet/output 
